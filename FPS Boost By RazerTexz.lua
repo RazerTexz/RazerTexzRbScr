d5cfe4fdeff4c4556df633ff4b228a7d3c955080
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2023-08-17 20:05:15 +0700

    Update FPS Boost By RazerTexz.lua

--- FPS Boost By RazerTexz.lua
+++ FPS Boost By RazerTexz.lua
@@ -1,8 +1,9 @@
 local parts = game:GetDescendants()
 for _, v in ipairs(parts) do
-	local success, error = pcall(function()
-		if v:IsA("MeshPart") or v:IsA("BasePart") or v:IsA("Part") then
-			v.Material = Enum.Material.SmoothPlastic
-		end
-	end)
+    local success, error = pcall(function()
+        if v:IsA("MeshPart") or v:IsA("BasePart") or v:IsA("Part") then
+	    v.Material = Enum.Material.SmoothPlastic
+		
+	end
+    end)
 end
