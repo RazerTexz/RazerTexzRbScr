5e9f4a5f2ac3579a4bdcf4e7918a063947c43872
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2023-08-17 19:57:08 +0700

    Create Change Jump Button Position.lua

---
+++ Change Jump Button Position.lua
@@ -0,0 +1,5 @@
+local players = game:GetService("Players")
+local localPlayer = players.LocalPlayer
+local jumpButton = localPlayer.PlayerGui.TouchGui.TouchControlFrame:FindFirstChild("JumpButton")
+
+jumpButton.Position = UDim2.fromScale(0.87, 0.72)
