37d252238453605b5a7d93ef1f2be92108cac4a1
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2023-12-24 08:11:08 +0700

    Add files via upload

--- FPS Boost By RazerTexz.lua
+++ FPS Boost By RazerTexz.lua
@@ -3,4 +3,7 @@ for _, v in getParts() do
 	if v:IsA("Part") or v:IsA("UnionOperation") or v:IsA("BasePart") or v:IsA("MeshPart") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
 		v.Material = Enum.Material.SmoothPlastic
 	end
+	if v:IsA("Model") then
+		v.LevelOfDetail = Enum.ModelLevelOfDetail.StreamingMesh
+	end
 end
