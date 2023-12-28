4932ba5b4eb4579ba9ac59ff9340bd80616a90ca
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2023-12-28 09:23:48 +0700

    Add files via upload

--- Kavo UI Draggable Android.lua
+++ Kavo UI Draggable Android.lua
@@ -136,12 +136,9 @@ local SettingsT = {}
 local settingsTPos = #SettingsT
 
 local Name = "KavoConfig.JSON"
-
 pcall(function()
-    if not pcall(function() readfile(Name) end) then
-        writefile(Name, game:service'HttpService':JSONEncode(SettingsT))
-    end
-    Settings = game:service'HttpService':JSONEncode(readfile(Name))
+    if not readfile(Name) then writefile(Name, game:GetService("HttpService"):JSONEncode(SettingsT)) end
+    Settings = game:GetService("HttpService"):JSONEncode(readfile(Name))
 end)
 
 local LibName = tostring(mathRandom(1, 100))..tostring(mathRandom(1,50))..tostring(mathRandom(1, 100))
@@ -466,13 +463,12 @@ function Kavo.CreateLib(kavName, themeList)
             for _, v in getSectionInners(sectionInners) do
                 while task.wait() do
                     if v:IsA("Frame") or v:IsA("TextButton") then
-                        local function size(pro)
+                        v.Changed:Connect(function(pro)
                             if pro == "Size" then
                                 UpdateSize()
                                 updateSectionFrame()
                             end
-                        end
-                        v.Changed:Connect(size)
+                        end)
                     end
                 end
             end
