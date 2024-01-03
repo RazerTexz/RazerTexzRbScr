51d36d6ef28d8502c1ddd7bce8f1b5ce971b9920
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2024-01-03 19:17:39 +0700

    Add files via upload

--- Chat Spy.lua
+++ Chat Spy.lua
@@ -1,40 +1,49 @@
-local enabled = true
-local spyOnMyself = false
-local public = false
-local publicItalics = false
+local color3FromRgb = Color3.fromRGB
+local udim2New = UDim2.new
+local udimNew = UDim.new
 
-local privateProperties = {
-	Color = Color3.fromRGB(0, 255, 255),
+-- This script reveals ALL hidden messages in the default chat
+local enabled = true -- Chat "/spy" to toggle!
+local spyOnMyself = true -- If true will check your messages too
+local public = false -- If true will chat the logs publicly (fun, risky)
+local publicItalics = true -- If true will use /me to stand out
+local privateProperties = { -- Customize private logs
+	Color = color3FromRgb(0, 255, 255),
 	Font = Enum.Font.SourceSansBold,
 	TextSize = 18
 }
 
 local starterGui = game:GetService("StarterGui")
-local players = game:GetService("Players")
-local localPlayer = players.LocalPlayer
-local saymsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
-local getmsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("OnMessageDoneFiltering")
+local plrs = game:GetService("Players")
+local getPlayers = plrs:GetPlayers()
+local player = plrs.LocalPlayer or plrs:GetPropertyChangedSignal("LocalPlayer"):Wait()
+local saymsg = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+local getmsg = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.OnMessageDoneFiltering
 local instance = (getgenv().chatSpyInstance or 0) + 1
 getgenv().chatSpyInstance = instance
 
-local function onChatted(player, msg)
-	if chatSpyInstance == instance then
-		if player == localPlayer and msg:lower():sub(1, 4) == "/spy" then
+local function onChatted(plr, msg)
+	if _G.chatSpyInstance == instance then
+		if plr == player and msg:lower():sub(1, 4) == "/spy" then
 			enabled = not enabled
+			task.wait(0.3)
 			privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
 			starterGui:SetCore("ChatMakeSystemMessage", privateProperties)
-		elseif enabled and (spyOnMyself or player ~= localPlayer) then
+		elseif enabled and (spyOnMyself or plr ~= player) then
 			msg = msg:gsub("[\n\r]",''):gsub("\t",' '):gsub("[ ]+",' ')
 			local hidden = true
-			local conn = getmsg.OnClientEvent:Connect(function(packet, channel)
-				if packet.SpeakerUserId == player.UserId and packet.Message == msg:sub(#msg - #packet.Message + 1) and (channel == "All" or (channel == "Team" and not public and players[packet.FromSpeaker].Team == localPlayer.Team)) then hidden = false end
+			local conn
+		    conn = getmsg.OnClientEvent:Connect(function(packet, channel)
+				if packet.SpeakerUserId == plr.UserId and packet.Message == msg:sub(#msg - #packet.Message + 1) and (channel == "All" or (channel == "Team" and not public and plrs[packet.FromSpeaker].Team == player.Team)) then hidden = false end
 			end)
+			task.wait(1)
 			conn:Disconnect()
+			conn = nil
 			if hidden and enabled then
 				if public then
-					saymsg:FireServer((publicItalics and "/me " or '').."{SPY} ["..player.Name.."]: "..msg, "All")
+					saymsg:FireServer((publicItalics and "/me " or '').."{SPY} ["..p.Name.."]: "..msg, "All")
 				else
-					privateProperties.Text = "{SPY} ["..player.Name.."]: "..msg
+					privateProperties.Text = "{SPY} ["..p.Name.."]: "..msg
 					starterGui:SetCore("ChatMakeSystemMessage", privateProperties)
 				end
 			end
@@ -42,16 +51,15 @@ local function onChatted(player, msg)
 	end
 end
 
-for _, plr in players:GetPlayers() do
-	plr.Chatted:Connect(function(msg) onChatted(plr, msg) end)
+for _, v in getPlayers do
+	v.Chatted:Connect(function(msg) onChatted(v, msg) end)
 end
-
-players.PlayerAdded:Connect(function(plr)
+plrs.PlayerAdded:Connect(function(plr)
 	plr.Chatted:Connect(function(msg) onChatted(plr, msg) end)
 end)
-
 privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
 starterGui:SetCore("ChatMakeSystemMessage", privateProperties)
-local chatFrame = localPlayer.PlayerGui.Chat.Frame
+if not player.PlayerGui:FindFirstChild("Chat") then task.wait(3) end
+local chatFrame = player.PlayerGui.Chat.Frame
 chatFrame.ChatChannelParentFrame.Visible = true
-chatFrame.ChatBarParentFrame.Position = chatFrame.ChatChannelParentFrame.Position + UDim2.new(UDim.new(), chatFrame.ChatChannelParentFrame.Size.Y)
+chatFrame.ChatBarParentFrame.Position = chatFrame.ChatChannelParentFrame.Position + udim2New(udimNew(), chatFrame.ChatChannelParentFrame.Size.Y)
