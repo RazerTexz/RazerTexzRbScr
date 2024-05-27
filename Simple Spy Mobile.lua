d8d73d1aa4531f2321997957c0a0228d84e54f4e
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2024-05-27 18:26:23 +0700

    Add files via upload

--- Simple Spy Mobile.lua
+++ Simple Spy Mobile.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-RemoteSpy-For-Mobile-6609"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RS/main/SimpleSpyMobile"))()
